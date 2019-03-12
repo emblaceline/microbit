9a6623c3f630126d8fb32a6469359488f19c7aa5
emblaceline <47786324+emblaceline@users.noreply.github.com>
2019-03-12 14:51:05 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,54 +1,18 @@
 #include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "ubit.h"
 
-#define GPIO ((NRF_GPIO_REGS*)0x50000000)
-
-typedef struct {
-	volatile uint32_t RESERVED0[321];
-	volatile uint32_t OUT;
-	volatile uint32_t OUTSET;
-	volatile uint32_t OUTCLR;
-	volatile uint32_t IN;
-	volatile uint32_t DIR;
-	volatile uint32_t DIRSET;
-	volatile uint32_t DIRCLR;
-	volatile uint32_t RESERVED1[120];
-	volatile uint32_t PIN_CNF[32];
-} NRF_GPIO_REGS;
+#include "bluetooth.h"
 
 int main(){
-	// Configure LED Matrix
-	for(int i = 4; i <= 15; i++){
-		GPIO->DIRSET = (1 << i);
-		GPIO->OUTCLR = (1 << i);
-	}
+	ubit_uart_init();
+	ubit_uart_print("Funker detta\n");
 
-	// Configure buttons
-	GPIO->PIN_CNF[__BUTTON_A_PIN__] = 0;
-	GPIO->PIN_CNF[__BUTTON_B_PIN__] = 0;
+	uint32_t err_code = 0;
 
-	int sleep = 0;
-	while(1){
-		
-|		/* Check if button B is pressed;
-		 * turn on LED matrix if it is. */
+	err_code = bluetooth_init();
+	ubit_uart_print("BLE Enable error code: %d\n\r", err_code);
 
-		if(!(GPIO->IN & (1<<26)) ) {
-			for(int i=13; i<=15 ; i++){
-				GPIO->OUTSET = (1 << I)
-			}
-		}
-
-		/* Check if button A is pressed;
-		 * turn off LED matrix if it is. */
-
-		if(!(GPIO->IN & (1<<17)) ) {
-			for(int i=13; i<=15 ; i++){
-				GPIO->OUTCLR = (1 << I)
-			}
-		}
-
-		sleep = 10000;
-		while(--sleep);
-	}
 	return 0;
 }
